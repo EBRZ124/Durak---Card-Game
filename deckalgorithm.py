28a8623e43612b9b309f1266f9bd1f28a738d508
EBRZ124 <130497524+EBRZ124@users.noreply.github.com>
2025-10-05 20:11:20 +0300

    Update deckalgorithm.py

--- deckalgorithm.py
+++ deckalgorithm.py
@@ -1,4 +1,5 @@
 import random
+import numpy as np
 
 diamonds = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]
 hearts = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]
@@ -10,36 +11,58 @@ opponent = []
 
 for i in range (0, 5):
     type = random.randint(0, 3)
-    card = random.randint(0, 12)
     if type == 0:
+        range = len(diamonds)
+        card = random.randint(0, range - 1)
+
         player.append(diamonds[card])
         diamonds.pop(card)
+
+        range = len(diamonds)
+        card = random.randint(0, range - 1)
+
+        opponent.append(diamonds[card])
+        diamonds.pop(card)
+
     if type == 1:
+        range = len(hearts)
+        card = random.randint(0, range - 1)
+
         player.append(hearts[card])
         hearts.pop(card)
+
+        range = len(hearts)
+        card = random.randint(0, range - 1)
+
+        opponent.append(hearts[card])
+        hearts.pop(card)
+
     if type == 2:
+        range = len(spades)
+        card = random.randint(0, range - 1)
+
         player.append(spades[card])
         spades.pop(card)
+
+        range = len(spades)
+        card = random.randint(0, range - 1)
+
+        opponent.append(spades[card])
+        spades.pop(card)
+
     if type == 3:
+        range = len(clubs)
+        card = random.randint(0, range - 1)
+
         player.append(clubs[card])
         clubs.pop(card)
 
+        range = len(clubs)
+        card = random.randint(0, range - 1)
 
-for i in range (0, 5):
-    type = random.randint(0, 3)
-    card = random.randint(0, 12)
-    if type == 0:
-        opponent.append(diamonds[card])
-        diamonds.pop(card)
-    if type == 1:
-        opponent.append(hearts[card])
-        hearts.pop(card)
-    if type == 2:
-        opponent.append(spades[card])
-        spades.pop(card)
-    if type == 3:
         opponent.append(clubs[card])
         clubs.pop(card)
+
 
 print(f"Your deck: {player}")
 print(f"Opponents deck: {opponent}")
