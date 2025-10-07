c2f5ede285b16f5e70a8398197198142fbf9c4b8
EBRZ124 <130497524+EBRZ124@users.noreply.github.com>
2025-10-07 12:39:11 +0300

    Update deckalgorithm.py

--- deckalgorithm.py
+++ deckalgorithm.py
@@ -1,46 +1,54 @@
 import random
 import numpy as np
 
-diamonds = ["♦2", "♦3", "♦4", "♦5", "♦6", "♦7", "♦8", "♦9", "♦10", "♦Jack", "♦Queen", "♦King", "♦Ace"]
-hearts = ["♥2", "♥3", "♥4", "♥5", "♥6", "♥7", "♥8", "♥9", "♥10", "♥Jack", "♥Queen", "♥King", "♥Ace"]
-spades = ["♠2", "♠3", "♠4", "♠5", "♠6", "♠7", "♠8", "♠9", "♠10", "♠Jack", "♠Queen", "♠King", "♠Ace"]
-clubs = ["♣2", "♣3", "♣4", "♣5", "♣6", "♣7", "♣8", "♣9", "♣10", "♣Jack", "♣Queen", "♣King", "♣Ace"]
+# diamonds 0 -> 12
+# hearts 13 - 25
+# spades 26 -> 38
+# clubs 39 -> 51
+the_deck = ["♦2", "♦3", "♦4", "♦5", "♦6", "♦7", "♦8", "♦9", "♦10", "♦Jack", "♦Queen", "♦King", "♦Ace",
+            "♥2", "♥3", "♥4", "♥5", "♥6", "♥7", "♥8", "♥9", "♥10", "♥Jack", "♥Queen", "♥King", "♥Ace",
+            "♠2", "♠3", "♠4", "♠5", "♠6", "♠7", "♠8", "♠9", "♠10", "♠Jack", "♠Queen", "♠King", "♠Ace",
+            "♣2", "♣3", "♣4", "♣5", "♣6", "♣7", "♣8", "♣9", "♣10", "♣Jack", "♣Queen", "♣King", "♣Ace"]
 
 trump = ["♦", "♥", "♠", "♣"]
 
 player = []
+player_attacks = ["", "", "", "", "", ""]
 opponent = []
+opponent_attacks = ["", "", "", "", "", ""]
+
+def player_card():
+    range = len(the_deck)
+    card = random.randint(0, range - 1)
+
+    opponent.append(the_deck[card])
+    the_deck.pop(card)
+
+def opponent_card():
+    range = len(the_deck)
+    card = random.randint(0, range - 1)
+
+    player.append(the_deck[card])
+    the_deck.pop(card)
 
 for i in range (0, 6):
-    type = random.randint(0, 3)
-    if type == 0:
-        type = diamonds
-    if type == 1:
-        type = hearts
-    if type == 2:
-        type = spades
-    if type == 3:
-        type = clubs
+    player_card()
+    opponent_card()
 
-    range = len(type)
-    card = random.randint(0, range - 1)
-
-    player.append(type[card])
-    type.pop(card)
-
-    range = len(type)
-    card = random.randint(0, range - 1)
-
-    opponent.append(type[card])
-    type.pop(card)
-
-cards_left = len(diamonds) + len(clubs) + len(spades) + len(hearts)
-print(cards_left)
+cards_left = len(the_deck)
 
 trump_cards = random.randint(0, 3)
 
 trump_type = trump[trump_cards]
 
+#print(cards_left)
 #print(f"Trump cards: {trump_type}")
 #print(f"Your deck: {player}")
 #print(f"Opponents deck: {opponent}")
+
+def check_card():
+    pass
+
+def place_card():
+    pass
+
